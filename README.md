15323284d9a15e39ddd44573bbf2e957d7f53ebc
wprajesh <rajeshit0610@gmail.com>
2023-08-29 16:06:01 +0530

    Swagger & Read me updated

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
-# lti-mindtree-music-api-challenge
-This is an assignment for LTI Mindtree
-Spotify Music API Challenge \
+# LTIMindtree Music Tracks API Challenge
 
-You can run the tool locally connecting to mysql.  \
-I'll keep this read me updated by evening.
+This project implements a REST API for storing and retrieving music track metadata using Node.js, Express, and Sequelize(DB ORM).
+
+The music data are consumed from Spotify API, you need to have a developer account with app created with credentials(App's client & secret keys) in Spotify.
+
+## Features
+
+- Create a new track with ISRC, title, and artist information.
+- Retrieve a track by its ISRC.
+- Retrieve tracks by artist name.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/wprajesh/lti-mindtree-music-api-challenge.git
+2. Navigate to the project directory:
+    ```bash
+    cd lti-mindtree-music-api-challenge
+3. Install dependencies:
+    ```bash
+    npm install
+4. Set up database configurations:
+    ```bash
+    DB_NAME=your_db_name
+    DB_USERNAME=your_db_username
+    DB_PASSWORD=your_db_password
+5. Set up Spotify credentials:
+    ```bash
+    CLIENT_ID=your_client_id
+    CLIENT_SECRET=your_client_secret
+    
+Add above details in .env file
+## Usage
+1. Start the server:
+    ```bash
+    npm start
+    The server will start on port 3000 by default.
+
+2. Access the API:
+    * To create a new track: Send a POST request to http://localhost:3000/tracks
+    * To get a track by ISRC: Send a GET request to http://localhost:3000/tracks/{isrc}
+    * To get tracks by artist name: Send a GET request to http://localhost:3000/tracks/artist/{artistName}
+
+## API Documentation
+The API is documented using Swagger. Access the documentation by starting the server and visiting http://localhost:3000/docs.
--- swagger/swaggerDef.js
+++ swagger/swaggerDef.js
@@ -5,7 +5,7 @@ const options = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'Music Metadata API',
+      title: 'LTI Mindtree Music API',
       version: '1.0.0',
     },
     paths: {
@@ -18,7 +18,7 @@ const options = {
                   schema: {
                     type: 'object',
                     properties: {
-                      ISRC: {
+                      isrc: {
                         type: 'string',
                         description: 'The ISRC of the track'
                       }
@@ -40,6 +40,9 @@ const options = {
               },
               '400': {
                 description: 'Bad request'
+              },
+              '409': {
+                description: 'Track already exists'
               },
               '500': {
                 description: 'Internal server error'
@@ -132,13 +135,51 @@ const options = {
                 type: 'string',
                 description: 'The title of the track'
               },
-              artist: {
-                type: 'string',
-                description: 'List of artist names'
+              Artists: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      type: 'integer',
+                      description: 'The ID of the artist'
+                    },
+                    name: {
+                      type: 'string',
+                      description: 'The name of the artist'
+                    },
+                    TrackIsrc: {
+                      type: 'string',
+                      description: 'The ISRC of the track associated with the artist'
+                    },
+                    updatedAt: {
+                      type: 'string',
+                      format: 'date-time',
+                      description: 'The timestamp when the record was updated'
+                    },
+                    createdAt: {
+                      type: 'string',
+                      format: 'date-time',
+                      description: 'The timestamp when the record was created'
+                    }
+                  }
+                },
+                description: 'List of artists associated with the track'
               },
-              imageURI: {
+              spotifyImageURI: {
                 type: 'string',
-                description: "The URI of the track's image"
+                format: 'uri',
+                description: "The URI of the track's album's Spotify image"
+              },
+              updatedAt: {
+                type: 'string',
+                format: 'date-time',
+                description: 'The timestamp when the record was updated'
+              },
+              createdAt: {
+                type: 'string',
+                format: 'date-time',
+                description: 'The timestamp when the record was created'
               }
             }
           }
